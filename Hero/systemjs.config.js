6bcbc8f85bf6cb2ad1282071ff2cddef8e0c63e6
Han Jia <hanj@datacom.co.nz>
2016-08-16 15:50:10 +1200

    rc4

--- Hero/systemjs.config.js
+++ Hero/systemjs.config.js
@@ -5,24 +5,30 @@
  */
 (function (global) {
 
-    var ngVer = '@2.0.0-rc.1'; // lock in the angular package version; do not let it float to current!
+    var ngVer = '@2.0.0-rc.4'; // lock in the angular package version; do not let it float to current!
+    var routerVer = '@3.0.0-beta.2'; // lock router version
+    var formsVer = '@0.2.0'; // lock forms version
+    var routerDeprecatedVer = '@2.0.0-rc.2'; // temporarily until we update all the guides
 
     //map tells the System loader where to look for things
     var map = {
         'app': 'hero-app',
 
         '@angular': 'https://npmcdn.com/@angular', // sufficient if we didn't pin the version
+        '@angular/router': 'https://npmcdn.com/@angular/router' + routerVer,
+        '@angular/forms': 'https://npmcdn.com/@angular/forms' + formsVer,
+        '@angular/router-deprecated': 'https://npmcdn.com/@angular/router-deprecated' + routerDeprecatedVer,
         'angular2-in-memory-web-api': 'https://npmcdn.com/angular2-in-memory-web-api', // get latest
         'rxjs': 'https://npmcdn.com/rxjs@5.0.0-beta.6',
         'ts': 'https://npmcdn.com/plugin-typescript@4.0.10/lib/plugin.js',
-        'typescript': 'https://npmcdn.com/typescript@1.8.10/lib/typescript.js',
+        'typescript': 'https://npmcdn.com/typescript@1.9.0-dev.20160409/lib/typescript.js',
     };
 
     //packages tells the System loader how to load when no filename and/or no extension
     var packages = {
         'app': { main: 'main.ts', defaultExtension: 'ts' },
         'rxjs': { defaultExtension: 'js' },
-        'angular2-in-memory-web-api': { defaultExtension: 'js' },
+        'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
     };
 
     var ngPackageNames = [
@@ -32,8 +38,6 @@
       'http',
       'platform-browser',
       'platform-browser-dynamic',
-      'router',
-      'router-deprecated',
       'upgrade',
     ];
 
@@ -42,16 +46,26 @@
     ngPackageNames.forEach(function (pkgName) {
         map['@angular/' + pkgName] = 'https://npmcdn.com/@angular/' + pkgName + ngVer;
     });
-
+    // Add moment
+    map['moment'] = 'node_modules/moment/moment.js';
     // Add package entries for angular packages
     ngPackageNames.forEach(function (pkgName) {
 
         // Bundled (~40 requests):
-        packages['@angular/' + pkgName] = { main: pkgName + '.umd.js', defaultExtension: 'js' };
+        packages['@angular/' + pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
 
         // Individual files (~300 requests):
         //packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
     });
+
+    // No umd for router yet
+    packages['@angular/router'] = { main: 'index.js', defaultExtension: 'js' };
+
+    // Forms not on rc yet
+    packages['@angular/forms'] = { main: 'index.js', defaultExtension: 'js' };
+
+    // Temporarily until we update the guides
+    packages['@angular/router-deprecated'] = { main: '/bundles/router-deprecated' + '.umd.js', defaultExtension: 'js' };
 
     var config = {
         // DEMO ONLY! REAL CODE SHOULD NOT TRANSPILE IN THE BROWSER
@@ -66,8 +80,15 @@
         },
         map: map,
         packages: packages
-    }
+    };
 
     System.config(config);
 
 })(this);
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
